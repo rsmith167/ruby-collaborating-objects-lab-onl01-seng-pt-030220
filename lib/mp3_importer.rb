367c86a3797df6a88a08a6d2674cc833cc9985ea
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 17:07:30 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,14 +1,14 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :files
   def initialize(path)
     @path = path
   end
   def files
     basedir = @path
-  files = Dir.glob("*.mp3")
+  @files = Dir.glob("*.mp3")
   end
   def import
-    
+    Song.new_by_file_name(@files)
   end
   
 end
