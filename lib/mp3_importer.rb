2eed6c137b6786b109d8ac9d5887f5eb7a4a3beb
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 17:23:57 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,19 @@
 class MP3Importer
-  attr_accessor :path, :files
+  attr_accessor :path, :files, :collect
   def initialize(path)
     @path = path
   end
   def files
   @files = Dir["./spec/fixtures/**/*.mp3"]
-  @files.collect {|x| 
+ @collect =  @files.collect {|x| 
   x.split("./spec/fixtures/mp3s/")[1]}
   
   end
   def import
-    Song.new_by_filename(@files)
+    
+    @collect.each{ |x| Song.new_by_filename(x) }
+    
+    
   end
   
 end
