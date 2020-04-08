1da2f3d123e68806772091d5af5a5889b8d351c8
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 17:22:04 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,7 +5,8 @@ class MP3Importer
   end
   def files
   @files = Dir["./spec/fixtures/**/*.mp3"]
-  @files.collect {|x| x.split()}
+  @files.collect {|x| 
+  x.split("./spec/fixtures/mp3s/")[1]}
   
   end
   def import
